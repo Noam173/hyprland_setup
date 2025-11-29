ec99954fa989af7765a7ba384d662aeac3fbf6a7
Noam Cohen <Noamiko173@gmail.com>
2025-11-30 00:55:31 +0200

    fixed the condition a little bit

--- main.py
+++ main.py
@@ -9,9 +9,9 @@ files: list[str] = glob(f"{Path(__file__).parent}/stuff/*/setup*")
 for i in files:
     run(f"python {i}", shell=True, check=True)
 run(f"clear && source {Path.home()}/.bashrc", shell=True, check=True)
-answer = input("launch linutil? [y/N] \n")
-if answer != "y":
+q1: str = input("launch linutil? [y/N] \n")
+if q1.lower() == 'y':
     run("curl -fsSL https://christitus.com/linux | sh", shell=True, check=True)
-answer = input("launch hyprland? [Y/n] \n")
-if answer != "n":
+q2: str = input("launch hyprland? [Y/n] \n")
+if q2 != "n":
     run("hyprland", shell=True, check=True)
