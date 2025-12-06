a177abc82f06c63cad01fe1068bd68cbaa6ea0f0
Noam Cohen <noamiko173@gmail.com>
2025-12-06 17:26:37 +0200

    ruff fixes

--- config/paru/setup_paru.py
+++ config/paru/setup_paru.py
@@ -1,14 +1,13 @@
-from glob import glob
 from pathlib import Path
 from shutil import copy
-from subprocess import run
 
 
 def main() -> None:
     path: Path = Path.home() / ".config" / "paru"
     path.mkdir(parents=True, exist_ok=True)
     idk: Path = Path(__file__).parent
-    copy(src=f"{idk}/paru.conf",dst=path)
+    copy(src=f"{idk}/paru.conf", dst=path)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
